52d14e86988edab712e8978d9d8c66ae337990ae
sharadbrat <sharoobrazno@mail.ru>
2019-06-14 00:14:15 +0200

    added impl of about page

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { match } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import { ArticleService } from '../../service';
-
-
-import './Article.scss';
-import { Link } from 'react-router-dom';
 import { BlogArticleModel } from '../../util';
 import { Loader } from '../../components/Loader';
+
+import './Article.scss';
 
 interface ArticleProps {
   match: match
@@ -28,15 +27,17 @@ export class Article extends React.Component<ArticleProps, ArticleState> {
   render() {
     let article = <div className="article__loader"><Loader/></div>;
 
+    // todo: add status
+
     if (this.state.article) {
       article = (
         <React.Fragment>
           <header className="article__header">
             <div className="article__header-container">
               <Link to="/blog" className="article__header-back" aria-label="Go back"/>
+              <h1 className="article__heading">{this.state.article.title}</h1>
             </div>
           </header>
-          <h1 className="article__heading">{this.state.article.title}</h1>
           <main className="article__main" dangerouslySetInnerHTML={{__html: this.state.article.text}}/>
         </React.Fragment>
       );
