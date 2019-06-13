96a57da3c5edd2ff895f9fcefaba67f8d31698c1
sharadbrat <sharoobrazno@mail.ru>
2019-06-13 16:16:47 +0200

    added footer component

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -3,7 +3,7 @@ import { Redirect } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
 import { ArticleService } from '../../service';
-import { BlogArticle, Loader } from '../../components';
+import { BlogArticle, Footer, Loader } from '../../components';
 import { BlogArticleModel, getLinkForNavigationItem, getNavigationItems, ResourceStatus } from '../../util';
 
 import './Blog.scss';
@@ -85,17 +85,7 @@ export class Blog extends React.Component<any, BlogState> {
               <p className="blog__description">Here you can find all my articles that are related to the frontend stuff and IT world in general. All the articles are published in medium, so if you have some preferences to the particular article platform, find medium link for the article in the very beginning of the article itself.</p>
               { articles }
             </main>
-            <footer className="blog__footer">
-              <div className="blog__footer-container">
-                <h2 className="blog__footer-heading">Find me everywhere:</h2>
-                <div className="blog__footer-links">
-                  <a className="blog__footer-link _twitter" href="https://twitter.com/GeorgiiSharadze" target="_blank" aria-label="Twitter"/>
-                  <a className="blog__footer-link _github" href="https://github.com/sharadbrat" target="_blank" aria-label="Github"/>
-                  <a className="blog__footer-link _telegram" href="https://t.me/sharadbrat" target="_blank" aria-label="Telegram"/>
-                  <a className="blog__footer-link _email" href="mailto:george.sharadze@gmail.com" aria-label="Email"/>
-                </div>
-              </div>
-            </footer>
+            <Footer/>
           </div>
         </section>
       </CSSTransition>
